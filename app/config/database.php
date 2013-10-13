b0776fd8e1bc1845802d58e10e7ab15384ce166f
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-10-13 14:56:11 +0300

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'games',
 			'username'  => 'root',
-			'password'  => 'wimble',
+			'password'  => 'falanfilan',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
