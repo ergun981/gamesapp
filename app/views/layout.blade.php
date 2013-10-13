6fa725bf89dcd304c5e461f2f42aabd3fd101a00
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-10-13 16:41:24 +0300

    Update layout.blade.php

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Games Collection</title>
+    <title>Oyunlar Koleksiyonu</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
 </head>
 <body>
     <div class="container">
         <nav class="navbar navbar-default" role="navigation">
             <div class="navbar-header">
-                <a href="{{ action('GamesController@index') }}" class="navbar-brand">Games Collection</a>
+                <a href="{{ action('GamesController@index') }}" class="navbar-brand">Oyunlar Koleksiyonu</a>
             </div>
         </nav>
         @yield('content')
