ab31a1312d35655c922e0146e1805f4862d8cf97
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-10-13 14:55:17 +0300

    Update GamesController.php

--- app/controllers/GamesController.php
+++ app/controllers/GamesController.php
@@ -6,7 +6,7 @@ class GamesController extends BaseController
 {
     public function index()
     {
-        // Show a listing of games.
+        // Oyunların listesini göster.
         $games = Game::all();
 
         return View::make('index', compact('games'));
@@ -14,13 +14,13 @@ class GamesController extends BaseController
 
     public function create()
     {
-        // Show the create game form.
+        // Oyun oluşturma formunu göster.
         return View::make('create');
     }
 
     public function handleCreate()
     {
-        // Handle create form submission.
+        // Oluşturma formu gönderimini işle.
         $game = new Game;
         $game->title        = Input::get('title');
         $game->publisher    = Input::get('publisher');
@@ -32,13 +32,13 @@ class GamesController extends BaseController
 
     public function edit(Game $game)
     {
-        // Show the edit game form.
+        // Oyun düzenleme formunu göster.
         return View::make('edit', compact('game'));
     }
 
     public function handleEdit()
     {
-        // Handle edit form submission.
+        // Düzenleme formu gönderimini işle.
         $game = Game::findOrFail(Input::get('id'));
         $game->title        = Input::get('title');
         $game->publisher    = Input::get('publisher');
@@ -50,13 +50,13 @@ class GamesController extends BaseController
 
     public function delete(Game $game)
     {
-        // Show delete confirmation page.
+        // Sil teyiti sayfasını göster.
         return View::make('delete', compact('game'));
     }
 
     public function handleDelete()
     {
-        // Handle the delete confirmation.
+        // Sil teyidini işle.
         $id = Input::get('game');
         $game = Game::findOrFail($id);
         $game->delete();
