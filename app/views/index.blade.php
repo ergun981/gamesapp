087decc821c7e8833e1fdecc5a314998057b86a2
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-10-13 16:40:32 +0300

    Update index.blade.php

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
     <div class="page-header">
-        <h1>All Games <small>Gotta catch 'em all!</small></h1>
+        <h1>Tüm Oyunlar<small> Hepsini yakalamak lazım!</small></h1>
     </div>
 
     <div class="panel panel-default">
         <div class="panel-body">
-            <a href="{{ action('GamesController@create') }}" class="btn btn-primary">Create Game</a>
+            <a href="{{ action('GamesController@create') }}" class="btn btn-primary">Oyun Oluştur</a>
         </div>
     </div>
 
     @if ($games->isEmpty())
-        <p>There are no games! :(</p>
+        <p>Hiçbir oyun yok! :(</p>
     @else
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>Title</th>
-                    <th>Publisher</th>
-                    <th>Complete</th>
-                    <th>Actions</th>
+                    <th>Adı</th>
+                    <th>Yayıncısı</th>
+                    <th>Tamamlanmış</th>
+                    <th>Eylemler</th>
                 </tr>
             </thead>
             <tbody>
@@ -28,10 +28,10 @@
                 <tr>
                     <td>{{ $game->title }}</td>
                     <td>{{ $game->publisher }}</td>
-                    <td>{{ $game->complete ? 'Yes' : 'No' }}</td>
+                    <td>{{ $game->complete ? 'Evet' : 'Hayır' }}</td>
                     <td>
-                        <a href="{{ action('GamesController@edit', $game->id) }}" class="btn btn-default">Edit</a>
-                        <a href="{{ action('GamesController@delete', $game->id) }}" class="btn btn-danger">Delete</a>
+                        <a href="{{ action('GamesController@edit', $game->id) }}" class="btn btn-default">Düzenle</a>
+                        <a href="{{ action('GamesController@delete', $game->id) }}" class="btn btn-danger">Sil</a>
                     </td>
                 </tr>
                 @endforeach
