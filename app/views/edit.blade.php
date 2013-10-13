f1a40c91adc0f01612c9733bb145b843ed35ca8e
Sinan Eldem <sinan@sinaneldem.com.tr>
2013-10-13 16:38:09 +0300

    Update edit.blade.php

--- app/views/edit.blade.php
+++ app/views/edit.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
     <div class="page-header">
-        <h1>Edit Game <small>Go on, mark it complete!</small></h1>
+        <h1>Oyun Düzenle<small> Devam et, tamamlandı olarak işaretle!</small></h1>
     </div>
 
     <form action="{{ action('GamesController@handleEdit') }}" method="post" role="form">
         <input type="hidden" name="id" value="{{ $game->id }}">
 
         <div class="form-group">
-            <label for="title">Title</label>
+            <label for="title">Adı</label>
             <input type="text" class="form-control" name="title" value="{{ $game->title }}" />
         </div>
         <div class="form-group">
-            <label for="publisher">Publisher</label>
+            <label for="publisher">Yayıncısı</label>
             <input type="text" class="form-control" name="publisher" value="{{ $game->publisher }}" />
         </div>
         <div class="checkbox">
             <label for="complete">
-                <input type="checkbox" name="complete" {{ $game->complete ? 'checked' : '' }} /> Complete?
+                <input type="checkbox" name="complete" {{ $game->complete ? 'checked' : '' }} /> Tamamlandı mı?
             </label>
         </div>
-        <input type="submit" value="Save" class="btn btn-primary" />
-        <a href="{{ action('GamesController@index') }}" class="btn btn-link">Cancel</a>
+        <input type="submit" value="Kaydet" class="btn btn-primary" />
+        <a href="{{ action('GamesController@index') }}" class="btn btn-link">İptal</a>
     </form>
 @stop
 
